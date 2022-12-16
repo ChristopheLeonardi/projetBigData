8e0b345623bb818725e39696311bd9ca8d2c548a
christophe leonardi <c.leonardi@live.fr>
2022-12-16 11:55:12 +0100

    kafka work

--- work/kafka-producer.py
+++ work/kafka-producer.py
@@ -1,44 +1,45 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[ ]:
+
+
+
+
+
 # In[1]:
 
 
-#get_ipython().system('pip install kafka-python')
+import json
+f = open('usr/sources.json')
+data_to_search = json.load(f)
+  
+f.close()
 
 
 # In[ ]:
 
 
+
+
+
+# In[3]:
+
+
 ## producer.py
 from kafka import KafkaProducer
-import json
-import numpy as np
 import time
 import requests
 
 p = KafkaProducer(bootstrap_servers=['kafka:9093'])
-topic = "topic_SNCF"
 
-url = "https://ressources.data.sncf.com/api/records/1.0/search/?dataset="
-dataset_name = "ponctualite-mensuelle-transilien"
-param = {
-    "rows" : "-1",
-    "sort" : "date",
-    "facet": ["date", "service", "ligne"],
-}
-def createQuery(url, dataset_name, param):
-    temp = ""
-    for item in param["facet"]:
-        temp += "&facet=" + item
-    query = url + dataset_name + "&q=" + "&rows=" + param["rows"] + "&sort=" + param["sort"] + temp
-    return query
+bol = True
+while bol == True:
+    for query in data_to_search:
+        data = requests.get(query["url"]).content
+        p.send(query["dataset_name"], data)
+        p.flush()
 
-while True:
-    #time.sleep(10)
-    messageAPI = requests.get(createQuery(url, dataset_name, param))
-    print(messageAPI.content)
-    result = p.send(topic, messageAPI.content)
 
 
 # In[ ]:
--- work/project.py
+++ work/project.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[9]:
+# In[ ]:
 
 
 
@@ -10,34 +10,32 @@
 # In[1]:
 
 
-from pyspark.sql import SparkSession
-spark = SparkSession.builder.appName("Projet Big Data").getOrCreate()
+import json
+f = open('usr/sources.json')
+data_to_search = json.load(f)
+  
+f.close()
 
 
-# In[4]:
+# In[6]:
 
 
 ## consumer.py
 from kafka import KafkaConsumer
 import json
-import numpy as np
 
-sortie_dict = []
-topic="topic_SNCF"
-consumer = KafkaConsumer(topic, bootstrap_servers=['kafka:9093'], group_id="my_group", auto_offset_reset="earliest")
+topics = [topic["dataset_name"] for topic in data_to_search]
+    
+kafkaConsumer = KafkaConsumer(bootstrap_servers=['kafka:9093']);
+kafkaConsumer.subscribe(topics);
 
-for msg in consumer:
-    sortie_dict = json.loads(msg.value.decode("utf-8"))
+for msg in kafkaConsumer:
+    print(dict(json.loads(msg.value)))
+
 
 
 # In[ ]:
 
 
-from pyspark.sql.types import *
-from pyspark.sql import functions as F
-from pyspark.sql.window import Window
-from pyspark.sql.functions import *
-
-#sortie_dict
-df = spark.read.format('json').options(header=True).load(sortie_dict)
+print(sortie_msg)
 
